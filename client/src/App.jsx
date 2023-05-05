437dba7e423a7f4af35d1673c72d67410cefdc89
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-05-05 12:08:08 +0100

    feat: scaffolded the react app for the client server

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className=' bg-slate-600 text-2xl min-h-full'>
+      Hello
+      <div className=' h-60 bg-red-600'>HEYYYYY</div>
+    </div>
+  )
+}
+
+export default App;
