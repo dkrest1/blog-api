2842e16e5ae1770749db891dadb3af523cdd0a2c
Emmanuel Ayodeji <hahyordeji@gmail.com>
2023-05-06 22:36:37 +0100

    update: updated navbar, setup redux toolkit and updated dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import Home from './Components/Home';
 import SignUpForm from './Components/SignUp';
 import Dashboard from './Components/Dashboard';
 import Footer from './Components/Footer';
+import { useState } from 'react';
 
 
 function App() {
 
+
   return (
     <div className="App flex flex-col min-h-screen">
       <BrowserRouter>
-        <Navbar/>
+        {/* <Navbar/> */}
         
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/" element={<Home />}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/sign-up' element={<SignUpForm/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {store} from './Components/redux/store'
+import {Provider} from 'react-redux'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
 )
