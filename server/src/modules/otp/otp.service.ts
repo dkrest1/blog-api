1eb7dda990b1ec9946efde7ad11e9f7c4f906073
Akande Oluwatosin <aktosin1@gmail.com>
2023-06-07 12:37:09 +0100

    resolve merge conflict

--- server/src/modules/otp/otp.service.ts
+++ server/src/modules/otp/otp.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Otp } from './entities/otp.entity';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+import { promisify } from 'util';
 
 @Injectable()
-export class OtpService {}
+export class OtpService {
+  constructor(
+    @InjectRepository(Otp)
+    private readonly otpRepository: Repository<Otp>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  private readonly transport = nodemailer.createTransport({
+    host: this.configService.get<string>('MAIL_HOST'),
+    port: parseInt(this.configService.get<string>('MAIL_PORT')),
+    auth: {
+      user: this.configService.get<string>('MAIL_USERNAME'),
+      pass: this.configService.get<string>('MAIL_PASSWORD'),
+    },
+  });
+}
